2b728d7b84cd8c778f672f6c5e4a8e935f498bb1
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-01 15:46:34 +0900

    Initial Commit

---
+++ Xcode-lesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Xcode-lesson
+//
+//  Created by 宮田亮 on 2020/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode-lesson/Xcode_lessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_lessonApp.swift
+//  Xcode-lesson
+//
+//  Created by 宮田亮 on 2020/12/01.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_lessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
