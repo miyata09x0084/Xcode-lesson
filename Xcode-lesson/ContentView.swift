41617159fb23df25cac22fb8deb03252e446c4f6
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-02 16:50:30 +0900

    ビューを定義する

--- Xcode-lesson/ContentView.swift
+++ Xcode-lesson/ContentView.swift
@@ -9,26 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("39424954871_e3afef0013_o")
-                .resizable()
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                .offset(x: -70, y: 0)
-                .frame(width: 250, height: 400)
-                .clipped()
-            Text("ほととぎす\n鳴きつるほうをながむれば\nただ有明の月ぞ残れる\n")
-                .font(.title)
-                .fontWeight(.light)
-                .foregroundColor(Color.white)
+        HStack {
+            Flower()
+            Hello()
+                .font(.system(size: 40))
                 .padding()
-                .offset(x: 0, y: -5)
-                .frame(width: 250, height: 400)
-                }
-        .rotation3DEffect(
-            .degrees(45), axis: (x: 1, y: 0, z: 0)
-            )
-        
+            Flower()
         }
+        .frame(height: 100)
+        .padding()
+    }
+}
+
+
+struct Hello: View {
+    var body: some View {
+        Text("Hello")
+            .font(.largeTitle)
+            .fontWeight(.thin)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.pink/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Flower: View {
+    var body: some View {
+        Image("39424954871_e3afef0013_o")
+          .resizable()
+            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
