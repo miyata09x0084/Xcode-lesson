a9093b9857dfd3eb52a58cd0341c554536741fbd
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-03 13:20:50 +0900

    配列リストを表示

--- Xcode-lesson/ContentView.swift
+++ Xcode-lesson/ContentView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
+let metro = ["銀座線", "丸の内線","日比谷線", "東西線","千代田線", "半蔵門線","南北線", "副都心線", ]
+
 struct ContentView: View {
     var body: some View {
-        List {
-            Text("Content 1")
-            Text("Content 2")
-            Text("Content 3")
-            Text("Content 4")
-            Text("Content 5")
-            Text("Content 6")
+        List(0 ..< metro.count) { item in
+            HStack {
+                Text(String(item))
+                Text(metro[item])
+            }
         }
     }
 }
