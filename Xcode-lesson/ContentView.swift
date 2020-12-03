a9c9b55d1178963f81adc405058464f0eae9d0d8
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-03 13:42:09 +0900

    複数のセクションがあるリスト

--- Xcode-lesson/ContentView.swift
+++ Xcode-lesson/ContentView.swift
@@ -7,16 +7,28 @@
 
 import SwiftUI
 
-let metro = ["銀座線", "丸の内線","日比谷線", "東西線","千代田線", "半蔵門線","南北線", "副都心線", ]
-
 struct ContentView: View {
+    let shikoku = ["徳島", "香川","愛媛", "高知" ]
+    let kyusyu = ["福岡", "佐賀","長崎", "熊本" ]
+    
     var body: some View {
-        List(0 ..< metro.count) { item in
-            HStack {
-                Text(String(item))
-                Text(metro[item])
+        List {
+            Section(header: Text("四国")
+                        .font(.largeTitle).padding(.top),
+                    footer: Text("最高標高は石山の1,982m")) {
+                ForEach( 0..<shikoku.count) { index in
+                    Text(self.shikoku[index])
+                }
+            }
+            Section(header: Text("九州")
+                        .font(.largeTitle).padding(.top),
+                    footer: Text("最高標高は富野裏山の1,936m")) {
+                ForEach( 0..<kyusyu.count) { index in
+                    Text(self.kyusyu[index])
+                }
             }
         }
+        .listStyle(GroupedListStyle())
     }
 }
 
