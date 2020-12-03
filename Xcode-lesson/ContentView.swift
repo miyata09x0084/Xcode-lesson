c17fa7b5e8dc370481fbc881e7be5c245b2d5f9a
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-03 16:10:36 +0900

    ボタンで実行する

--- Xcode-lesson/ContentView.swift
+++ Xcode-lesson/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var num:Int = 0
+    
     var body: some View {
-        NavigationView {
-            List(photoArray) { item in
-                NavigationLink(destination: PhotoDetailView(photo: item)) {
-                        PhotoRaw(photo: item)
-                    }
+        VStack {
+            Button(action: {
+                self.num = Int.random(in: 0...100)
+                print(num)
+            }) {
+                Text("Random Button")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 280.0, height: 60.0)
+                    .background(Color.blue)
+                    .cornerRadius(15, antialiased: true)
+                
             }
-            .navigationBarTitle(Text("写真リスト"))
+            Text("\(num)")
+                .font(.largeTitle)
+                .padding()
         }
     }
 }
