2ccda0e5c098deb8df26aaa9c9d867a7d26d51df
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2020-12-07 14:16:25 +0900

    キーボードでテキストフィールド入力

--- Xcode-lesson/ContentView.swift
+++ Xcode-lesson/ContentView.swift
@@ -7,31 +7,70 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State var theDate = Date()
-    @Environment(\.locale) var locale:Locale
-    
-    var dateformate: DateFormatter {
-        let df = DateFormatter()
-        df.locale = Locale(identifier: "ja_JP")
-        df.dateStyle = .full
-        df.timeStyle = .short
-        return df
+extension UIApplication {
+    func endEditing() {
+        sendAction(
+            #selector(UIResponder.resignFirstResponder),
+            to: nil, from: nil, for: nil
+        )
     }
+}
+
+struct ContentView: View {
+    @State var kosu:String = ""
+    let tanka:Double = 250
+    let tax:Double = 1.1
 
     var body: some View {
-        Form {
-            Text(dateformate.string(from: theDate)).font(.headline)
-            DatePicker(selection: $theDate,
-                       displayedComponents: .date,
-                       label: { Text("日時") })
-                .environment(\.locale, Locale(identifier: "ja_JP"))
-            DatePicker(selection: $theDate,
-                       displayedComponents: .hourAndMinute,
-                       label: { Text("日時") })
-                .environment(\.locale, Locale(identifier: "ja_JP"))
-        }.padding()
+        ZStack {
+            Color.white
+                .onTapGesture {
+                    UIApplication.shared.endEditing()
+                }
+            
+           VStack (alignment: .leading) {
+                HStack {
+                    Text("個数：").padding(.horizontal, 0)
+                    TextField("0", text: $kosu)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .keyboardType(.numberPad)
+                        .frame(width: 80)
+                }
+                
+                .font(.title)
+                .frame(width: 300)
+                
+                Group {
+                    if kosuCheck(min: 1, max: 10) {
+                        Text("\(price())円です。")
+                            .font(.title)
+                    } else {
+                        Text("個数は1〜10個を入れてください。")
+                            .foregroundColor(.red)
+                            .font(.headline)
+                    }
+                }.frame(width: 400, height: 30)
+            }
+            .position(x:200, y:200)
+        }
     }
+    
+    func kosuCheck(min:Int, max:Int) -> Bool {
+        guard let num = Int(kosu) else {
+            return false
+        }
+        return (min...max).contains(num)
+    }
+    
+    func price() -> Int {
+        if let num = Double(kosu) {
+            let result = Int(tanka * num * tax)
+            return result
+        } else {
+            return -1
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
